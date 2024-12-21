dd45b7390f0377efa6a48bbdf4e26af523ea1915
silvieon <ywang1051@gmail.com>
2024-12-21 12:27:37 -0500

    Added
    	modified:   about/index.html
    	renamed:    assets/components/footer-homepage.js -> components/footer-homepage.js
    	renamed:    assets/components/footer.css -> components/footer.css
    	renamed:    assets/components/footer.js -> components/footer.js
    	renamed:    assets/components/header-homepage.js -> components/header-homepage.js
    	renamed:    assets/components/header.css -> components/header.css
    	renamed:    assets/components/header.js -> components/header.js
    	renamed:    assets/components/navbar-homepage.js -> components/navbar-homepage.js
    	renamed:    assets/components/navbar.css -> components/navbar.css
    	renamed:    assets/components/navbar.js -> components/navbar.js
        new file:   home/index.html
    	new file:   home/style.css
    	modified:   index.html
    	modified:   join/index.html
    	modified:   media/index.html
    	modified:   shop/index.html
    	modified:   shows/index.html

--- components/navbar.js
+++ components/navbar.js
@@ -11,7 +11,6 @@ class Navbar extends HTMLElement {
             <li><a id="nav_link" href="../shows">shows</a></li>
             <li><a id="nav_link" href="../media">media</a></li>
             <li><a id="nav_link" href="../about">about</a></li>
-            <li><a id="nav_link" href="../join">join</a></li>
             <li><a id="nav_link" href="../shop">shop</a></li>
             </ul>
         </nav>
