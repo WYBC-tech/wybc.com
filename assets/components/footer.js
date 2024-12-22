3d39b753fa78d79527db573059d1312b962ad595
silvieon <ywang1051@gmail.com>
2024-12-21 23:15:30 -0500

    modified:   about/index.html 	renamed:    components/footer.css -> assets/components/footer.css 	new file:   assets/components/footer.js 	renamed:    components/header.css -> assets/components/header.css 	renamed:    components/header.js -> assets/components/header.js 	renamed:    components/navbar.css -> assets/components/navbar.css 	renamed:    components/navbar.js -> assets/components/navbar.js 	modified:   assets/style.css 	deleted:    components/footer.js 	new file:   contact/index.html 	renamed:    join/style.css -> contact/style.css 	modified:   home/index.html 	modified:   index.html 	modified:   media/index.html 	modified:   shop/index.html 	modified:   shows/index.html 	modified:   template.html 	renamed:    join/index.html -> unused-content (currently)/join/index.html 	new file:   unused-content (currently)/join/style.css

---
+++ assets/components/footer.js
@@ -0,0 +1,31 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="footerdiv">
+            <div class="socials">
+                <div id="social">
+                <img src="../assets/images/icons/instagram.png" id="socials_icon">
+                <a id="social_link" href="https://www.instagram.com/wybcyale/" target="_blank">@WYBCYALE</a>
+                </div>
+
+                <div id="social">
+                <img src="../assets/images/icons/facebook.png" id="socials_icon">
+                <a id="social_link" href="https://www.facebook.com/yaleradio/" target="_blank">@YALERADIO</a>
+                </div>
+            </div>
+
+            <div class="contacts">
+                <div id="contact">
+                <a id="contact_link" href="../contact/">Contact</a>
+                </div>
+            </div>
+        </footer>
+        `;
+    }
+}
+
+customElements.define('site-footer', Footer);
