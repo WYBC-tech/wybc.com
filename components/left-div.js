42e79a7b576023aa244f5feba60b4f442bd7ddff
WYBC-tech <tech@wybc.com>
2025-01-03 21:20:24 +1300

    ugh

--- components/left-div.js
+++ components/left-div.js
@@ -14,7 +14,6 @@ class LeftDiv extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-        <div class="column_left">
             <logo-block></logo-block>
             <div class="box">
                 <div class="navdiv">
@@ -28,7 +27,6 @@ class LeftDiv extends HTMLElement {
                 </nav>
                 </div>
             </div
-        </div>
         `;
     }
 }
