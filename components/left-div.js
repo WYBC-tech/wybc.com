ad8acc8d1f6d082ad0b10bc2d723946a29ea06ba
WYBC-tech <tech@wybc.com>
2025-01-03 21:14:59 +1300

    testing left div template

---
+++ components/left-div.js
@@ -0,0 +1,36 @@
+/**
+ * Code for the left div/column, is the same on every page.
+ * 
+ * @class JavaScript custom component
+ * @argument pages currently in use. Be sure to update this list of links every
+ *           time a page is added or removed to the list of those currently in 
+ *           use. 
+ */
+
+class LeftDiv extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="column-left">
+            <logo-block></logo-block>
+            <div class="box">
+                <div class="navdiv">
+                <nav class="navbar">
+                <ul class="navlist">
+                    <li><a id="nav_link" href="../home/">home</a></li>
+                    <li><a id="nav_link" href="../programs/">programs</a></li>
+                    <li><a id="nav_link" href="../media/">media</a></li>
+                    <li><a id="nav_link" href="../about/">about</a></li>
+                </ul>
+                </nav>
+                </div>
+            </div
+        </div>
+        `;
+    }
+}
+
+customElements.define('left-div', LeftDiv);
