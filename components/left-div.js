12ea3495215ee3addc777fcac1feccf7396ad0a9
WYBC-tech <tech@wybc.com>
2025-01-03 21:40:54 +1300

    eek

--- components/left-div.js
+++ components/left-div.js
@@ -16,7 +16,16 @@ class LeftDiv extends HTMLElement {
         this.innerHTML = `
             <logo-block></logo-block>
             <div class="box">
-                <site-nav></site-nav>
+                <div class="navdiv">
+                <nav class="navbar">
+                    <ul class="navlist">
+                        <li><a id="nav_link" href="../home/">home</a></li>
+                        <li><a id="nav_link" href="../programs/">programs</a></li>
+                        <li><a id="nav_link" href="../media/">media</a></li>
+                        <li><a id="nav_link" href="../about/">about</a></li>
+                    </ul>
+                </nav>
+                </div>
             </div>
         `;
     }
