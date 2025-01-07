9a9a53ba2d6bdff414e2cc9f3b1837862266a1b2
WYBC-tech <tech@wybc.com>
2025-01-07 21:23:43 +1300

    weather widget take 1

--- components/left-div.js
+++ components/left-div.js
@@ -15,16 +15,23 @@ class LeftDiv extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <logo-block></logo-block>
+            <!--Weather widget-->
+            <div class="box">
+                <a class="weatherwidget-io" href="https://forecast7.com/en/41d31n72d93/new-haven/?unit=us" data-label_1="NEW HAVEN" data-label_2="WEATHER" data-font="Verdana" data-mode="Current" data-theme="kiwi" >NEW HAVEN, CT</a>
+                <script>
+                    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+                </script>                
+            </div>
             <div class="box">
                 <div class="navdiv">
-                <nav class="navbar">
-                    <ul class="navlist">
-                        <li><a id="nav_link" href="../home/">home</a></li>
-                        <li><a id="nav_link" href="../programs/">programs</a></li>
-                        <li><a id="nav_link" href="../media/">media</a></li>
-                        <li><a id="nav_link" href="../about/">about</a></li>
-                    </ul>
-                </nav>
+                    <nav class="navbar">
+                        <ul class="navlist">
+                            <li><a id="nav_link" href="../home/">home</a></li>
+                            <li><a id="nav_link" href="../programs/">programs</a></li>
+                            <li><a id="nav_link" href="../media/">media</a></li>
+                            <li><a id="nav_link" href="../about/">about</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         `;
