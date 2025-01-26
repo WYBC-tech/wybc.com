0604d4de86faa2df95a73a3d7497bf018ca9f2dc
WYBC-tech <tech@wybc.com>
2025-01-26 15:56:39 -0500

    removed weather

--- components/left-div.js
+++ components/left-div.js
@@ -17,9 +17,8 @@ class LeftDiv extends HTMLElement {
         <div id="leftDivStructure">
             <logo-block id="logo-block"></logo-block>
             
-            <div id="weatherframe">
-                <div id="ww_a2708c6f56607" v='1.3' loc='id' a='{"t":"responsive","lang":"en","sl_lpl":1,"ids":["wl3490"],"font":"Arial","sl_ics":"one","sl_sot":"fahrenheit","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","el_wfc":3}'>More forecasts: <a href="https://oneweather.org/new_york_city/30_days/" id="ww_a2708c6f56607_u" target="_blank">New York 30 day forecast</a></div><script async src="https://app3.weatherwidget.org/js/?id=ww_a2708c6f56607"></script>
-            </div>
+           <!-- <div id="weatherframe">
+            </div>-->
 
             <div class="box">
                 <div id="navdiv">
