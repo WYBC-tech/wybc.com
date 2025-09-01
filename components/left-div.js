2628a620a2a45423fc1843e039a33d9b536bb1ae
nxing831 <nxing831@gmail.com>
2025-09-01 19:56:03 -0400

    changes to navigation, colors, etc.etc. it's a mess.

--- components/left-div.js
+++ components/left-div.js
@@ -14,27 +14,22 @@ class LeftDiv extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-        <div id="leftDivStructure">
-            <logo-block id="logo-block"></logo-block>
-            
-           <!-- <div id="weatherframe">
-            </div>-->
+                <nav>
+                    <ul>
+                        <li><a href="../home/">home</a></li>
+                    </ul>
 
-            <div class="box">
-                <div id="navdiv">
-                    <nav>
-                        <ul>
-                            <li><a href="../home/">home</a></li>
-                            <li><a href="../programs/">programs</a></li>
-                            <li><a href="../sports/">sports</a></li>
-                            <li><a href="../media/">media</a></li>
-                            <li><a href="../about/">about</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
+                    <ul>
+                        <li><a href="../programs/">programs</a></li>
+                        <li><a href="../sports/">sports</a></li>
+                    </ul>
 
+                    <ul>
+                        <li><a href="../media/">media</a></li>
+                        <li><a href="../about/">about</a></li>
+                    </ul>
+
+                </nav>
         `;
     }
 }
